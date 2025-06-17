0382a9de1782f57ca5c771ede25a4fa6a149df74
Kevin CS <kevinchandra031@gmail.com>
2025-06-17 23:37:37 +0700

    Update sign in page.

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,7 +1,13 @@
-export default function SignIn() {
+import { Head1, Head2, Head3, Head4, Par } from '@/components/ui/typography'
+
+export default function Profile() {
   return (
     <>
-      Sign In Page
+      <Head1>Lorem ipsum</Head1>
+      <Head2>Dolor sit amet</Head2>
+      <Head3>Consectetur</Head3>
+      <Head4>Adipisicing elit</Head4>
+      <Par>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam repudiandae explicabo at tempora consequuntur architecto sunt obcaecati quidem velit eligendi hic, vero nostrum rerum tenetur sequi, ipsam amet itaque dolor.</Par>
     </>
   )
 }
