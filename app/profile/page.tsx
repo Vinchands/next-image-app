bd0fbd62eaa9a85d8e9558812cf32603ea3190c0
Kevin CS <kevinchandra031@gmail.com>
2025-06-17 23:37:18 +0700

    Create profile page for demo.

---
+++ app/profile/page.tsx
@@ -0,0 +1,13 @@
+import { Head1, Head2, Head3, Head4, Par } from '@/components/ui/typography'
+
+export default function Profile() {
+  return (
+    <>
+      <Head1>Lorem ipsum</Head1>
+      <Head2>Dolor sit amet</Head2>
+      <Head3>Consectetur</Head3>
+      <Head4>Adipisicing elit</Head4>
+      <Par>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam repudiandae explicabo at tempora consequuntur architecto sunt obcaecati quidem velit eligendi hic, vero nostrum rerum tenetur sequi, ipsam amet itaque dolor.</Par>
+    </>
+  )
+}
