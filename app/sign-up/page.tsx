0564eb89dd835aa041d0f8a0c93f7c730829db06
Kevin CS <kevinchandra031@gmail.com>
2025-06-17 23:38:50 +0700

    Implement sign up form to the page.

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,7 +1,21 @@
-export default function SingUp() {
+import SignupForm from './SignupForm'
+import bg from '@/public/bg.jpg'
+
+export default function Signup() {
   return (
-    <div className="min-h-[75vh] flex items-center justify-center">
-      
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2 justify-center">
+      <div className="flex items-center justify-center inset-0">
+        <SignupForm />
+      </div>
+      <div
+        style={{
+          backgroundImage: `url('${bg.src}')`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundAttachment: 'fixed'
+        }}
+        className="hidden lg:inline"
+      />
     </div>
   )
 }
