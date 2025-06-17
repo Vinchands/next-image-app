9591efcd09e2c0dfc960a3fab7445745786c1942
Kevin CS <kevinchandra031@gmail.com>
2025-06-17 23:29:59 +0700

    Add functionality and validation schemas to the auth server actions.

--- actions/auth.ts
+++ actions/auth.ts
@@ -1,13 +1,125 @@
 'use server'
+import { z } from 'zod'
+import bcrypt from 'bcrypt'
+import prisma from '@/lib/prisma'
+import { createSession, deleteSession } from '@/lib/auth/session'
+import { redirect } from 'next/navigation'
 
-export async function signup() {
+const SignupSchema = z.object({
+  name: z
+    .string()
+    .min(2, { message: 'Name must be at least 2 characters long.' })
+    .trim(),
+  email: z
+    .string()
+    .email({ message: 'Please enter a valid email.' })
+    .trim(),
+  password: z
+    .string()
+    .min(8, { message: 'Password must be at least 8 characters long.' })
+    .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
+    .regex(/[0-9]/, { message: 'Contain at least one number.' })
+    .regex(/[^a-zA-Z0-9]/, {
+      message: 'Contain at least one special character.',
+    })
+    .trim(),
+})
+
+const SigninSchema = z.object({
+  email: z
+    .string()
+    .email({ message: 'Please enter a valid email.' })
+    .trim(),
+  password: z
+    .string()
+    .min(1, { message: 'Password is required.' })
+})
+
+export type FormState =
+  | {
+      errors?: {
+        name?: string[]
+        email?: string[]
+        password?: string[]
+      }
+      message?: string
+    }
+  | undefined
+
+export async function signup(state: FormState, formData: FormData) {
+  const validation = SignupSchema.safeParse(Object.fromEntries(formData))
   
+  if (!validation.success) {
+    return {
+      errors: validation.error.flatten().fieldErrors,
+    }
+  }
+  
+  const { name, email, password } = validation.data
+  
+  const user = await prisma.user.findFirst({
+    where: { email }
+  })
+  
+  if (user) {
+    return {
+      errors: {
+        password: ['Unable to sign up. Use different email or try again later.']
+      }
+    }
+  }
+  
+  const passwordHash = await bcrypt.hash(password, 10)
+  const newUser = await prisma.user.create({
+    data: {
+      name, email, password: passwordHash
+    }
+  })
+  
+  await createSession(newUser.id, 'refresh')
+  await createSession(newUser.id, 'access')
+  
+  redirect('/profile')
 }
 
-export async function signin() {
+export async function signin(state: FormState, formData: FormData) {
+  const validation = SigninSchema.safeParse(Object.fromEntries(formData))
   
+  if (!validation.success) {
+    return {
+      errors: validation.error.flatten().fieldErrors,
+    }
+  }
+  
+  const { email, password } = validation.data
+  const user = await prisma.user.findFirst({
+    where: { email }
+  })
+  
+  if (!user) {
+    return {
+      errors: {
+        password: ['Invalid email or password.']
+      }
+    }
+  }
+  
+  const isValid = await bcrypt.compare(password, user.password)
+  if (!isValid) {
+    return {
+      errors: {
+        password: ['Invalid email or password.']
+      }
+    }
+  }
+
+  await createSession(user.id, 'refresh')
+  await createSession(user.id, 'access')
+  
+  redirect('/profile')
 }
 
 export async function signout() {
-  
+  await deleteSession()
+  redirect('/sign-in')
 }
