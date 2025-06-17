5a3008714d189216e1b4204d3b0b35eba3cfc696
Kevin CS <kevinchandra031@gmail.com>
2025-06-17 23:29:19 +0700

    Create session management file and JWT helper functions.

---
+++ lib/auth/jwt.ts
@@ -0,0 +1,36 @@
+import 'server-only'
+import { SignJWT, jwtVerify } from 'jose'
+import type { SessionPayload, TokenType } from '@/lib/auth/definitions'
+
+const encodedAccessKey = new TextEncoder().encode(process.env.JWT_ACCESS_SECRET)
+const encodedRefreshKey = new TextEncoder().encode(process.env.JWT_REFRESH_SECRET)
+
+// Encrypt
+export async function generateToken(payload: SessionPayload, tokenType: TokenType) {
+  
+  const encodedKey = tokenType === 'access'? encodedAccessKey : encodedRefreshKey
+  const expirationTime = tokenType === 'access'? '1h' : '7d'
+  
+  return new SignJWT(payload)
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setExpirationTime(expirationTime)
+    .sign(encodedKey)
+}
+
+// Decrypt
+export async function parseToken(token: string | undefined = '', tokenType: TokenType) {
+  if (!token) return null
+  
+  const encodedKey = tokenType === 'access'? encodedAccessKey : encodedRefreshKey
+  
+  try {
+    const { payload } = await jwtVerify(token, encodedKey, {
+      algorithms: ['HS256'],
+    })
+    return payload
+  } catch (err) {
+    console.error(`Failed to verify token`, err)
+    return null
+  }
+}
---
+++ lib/auth/session.ts
@@ -0,0 +1,35 @@
+import 'server-only'
+import { generateToken, parseToken } from '@/lib/auth/jwt'
+import type { TokenType } from '@/lib/auth/definitions'
+import { cookies } from 'next/headers'
+
+export async function createSession(userId: string, type: TokenType) {
+  // * 1h for access token and 7d for refresh token
+  const expiresAt = new Date(
+    Date.now() + (type === 'access'? 1 : 7 * 24) * 3600 * 1000
+  )
+  const token = await generateToken({ userId, expiresAt }, type)
+  const cookieStore = await cookies()
+  
+  cookieStore.set(`${type}-token`, token, {
+    httpOnly: true,
+    secure: true,
+    expires: expiresAt,
+    sameSite: 'lax',
+    path: '/'
+  })
+}
+
+export async function deleteSession() {
+  const cookieStore = await cookies()
+  cookieStore.delete('access-token')
+  cookieStore.delete('refresh-token')
+}
+
+export async function regenerateAccessToken() {
+  const cookieStore = await cookies()
+  const accessToken = cookieStore.get('access-token')?.value
+  const payload = await parseToken(accessToken, 'access')
+  
+  if (!payload?.userId) return null
+}
