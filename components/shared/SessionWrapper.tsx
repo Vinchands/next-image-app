d214bde658ccb58e40509a1fcc05c98432b0a44d
Kevin CS <kevinchandra031@gmail.com>
2025-06-18 21:33:15 +0700

    Implement 'getSafeUser' function.

--- components/shared/SessionWrapper.tsx
+++ components/shared/SessionWrapper.tsx
@@ -1,12 +1,16 @@
 import { getAuthUser } from '@/lib/dal/user'
 import { SessionProvider } from '@/components/shared/SessionProvider'
+import { getSafeUser } from '@/lib/dto/user'
 
 export default async function SessionWrapper({ children }: { children: React.ReactNode }) {
   
   const user = await getAuthUser()
+  let safeUser = null
+  
+  if (!!user) safeUser = getSafeUser(user)
   
   return (
-    <SessionProvider value={{ isAuth: !!user, user }}>
+    <SessionProvider value={{ isAuth: !!safeUser, user: safeUser }}>
       {children}
     </SessionProvider>
   )
