1cfc6e33a445b9c5311b07159a7ccf343a5a0ad6
Kevin CS <kevinchandra031@gmail.com>
2025-06-18 21:31:07 +0700

    Create sign out button.

---
+++ components/ui/SignoutButton.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { cn } from '@/lib/utils'
+import ActionAlert from '@/components/ui/ActionAlert'
+import { Button } from '@/components/ui/button'
+import { signout } from '@/actions/auth'
+
+type SignoutButtonProps = {
+  children: React.ReactNode
+  className?: string
+}
+
+export default function SignoutButton({ children, className }: SignoutButtonProps) {
+  return (
+    <ActionAlert
+      title="Are you sure?"
+      renderTrigger={
+        <button className={cn('text-destructive', className)}>{children}</button>
+      }
+      renderAction={
+        <Button
+          variant="destructive"
+          onClick={async () => await signout()}
+        >
+          Yes
+        </Button>
+      }
+    />
+  )
+}
