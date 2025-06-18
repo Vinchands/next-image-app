d8bd396a375d69b8394da38e8ce432d493d8c8a5
Kevin CS <kevinchandra031@gmail.com>
2025-06-18 21:24:16 +0700

    Change 'min-h-screen' to 'min-h-[90vh]' for improved UI.

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -3,7 +3,7 @@ import bg from '@/public/bg.jpg'
 
 export default function Signup() {
   return (
-    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2 justify-center">
+    <div className="min-h-[90vh] grid grid-cols-1 lg:grid-cols-2 justify-center">
       <div className="flex items-center justify-center inset-0">
         <SignupForm />
       </div>
