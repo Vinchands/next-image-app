0784e1c6fff2759ff77552bbeb7d0d9f4d23c5a2
Kevin CS <kevinchandra031@gmail.com>
2025-06-18 21:27:48 +0700

    Rename file to 'PasswordInput.tsx' for naming consistency.

--- components/ui/PasswordInput.tsx
+++ components/ui/PasswordInput.tsx
@@ -24,7 +24,7 @@ function PasswordInput({ ...props }: Omit<React.ComponentProps<'input'>, 'type'>
         className="absolute right-2 text-gray-500 hover:text-gray-700"
         aria-label={showPassword ? 'Hide password' : 'Show password'}
       >
-        {showPassword ? <EyeClosed size={18} /> : <Eye size={18} />}
+        {showPassword ? <Eye size={18} /> : <EyeClosed size={18} />}
       </button>
     </div>
   )
