763513db5e0c1cf21a28e909bf54ded5c6e52caa
Kevin CS <kevinchandra031@gmail.com>
2025-06-18 21:34:29 +0700

    Implement 'MainNavigation' component.

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,39 +1,17 @@
-'use client'
 import Link from 'next/link'
 import Image from 'next/image'
-import { Button } from '@/components/ui/button'
-import { usePathname } from 'next/navigation'
-import { useCallback } from 'react'
+import MainNavigation from '@/components/shared/MainNavigation'
 import logo from '@/public/logo.svg'
 
 export default function Navbar() {
   
-  const path = usePathname()
-  
-  const isActive = useCallback((href: string) => {
-    return path === href
-  }, [path])
-  
   return (
     <header className="sticky top-0 flex items-center justify-between md:justify-around p-3 bg-card shadow">
-      <div className="flex items-center gap-x-2">
-        <Image src={logo} alt="imagenation" className="w-10" />
-        <span className="font-bold text-2xl">imagenation</span>
-      </div>
-      <nav className="flex items-center gap-x-3">
-        <Link href="/">
-          Home
-        </Link>
-        <Link href="/images">
-          Images
-        </Link>
-        <Link href="/sign-in">
-          Sign In
-        </Link>
-        <Button asChild>
-          <Link href="/sign-up">Join Us</Link>
-        </Button>
-      </nav>
+      <Link href="/" className="flex items-center gap-x-2">
+        <Image src={logo} alt="imagenation" className="w-8" />
+        <span className="hidden sm:inline font-bold text-2xl">imagenation</span>
+      </Link>
+      <MainNavigation />
     </header>
   )
 }
