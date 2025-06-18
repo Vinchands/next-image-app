1caf940e355d0c0d227507b6575a3a23589f7128
Kevin CS <kevinchandra031@gmail.com>
2025-06-18 21:30:52 +0700

    Create 'UserAvatar' component with dropdown menu.

---
+++ components/ui/UserAvatar.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuGroup,
+  DropdownMenuItem
+} from '@/components/ui/dropdown-menu'
+import SignoutButton from '@/components/ui/SignoutButton'
+import { getInitials } from '@/lib/utils'
+import { SafeUser } from '@/lib/definitions'
+import icon from '@/public/user.png'
+
+export default function UserAvatar({ user }: { user:  SafeUser }) {
+  
+  const { name, email, photoUrl } = user
+  
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar>
+          <AvatarImage src={photoUrl || icon.src} />
+          <AvatarFallback>{getInitials(name)}</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>
+          <p className="font-bold">{name}</p>
+          <span className="text-sm text-muted-foreground">{email}</span>
+        </DropdownMenuLabel>
+        <DropdownMenuGroup>
+          <DropdownMenuItem asChild>
+            <Link href="/profile">Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem variant="destructive" asChild>
+            <SignoutButton>Sign out</SignoutButton>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
