3a73de9c70e1dcae9390be8ca9967021fa47961f
Kevin CS <kevinchandra031@gmail.com>
2025-06-18 21:29:53 +0700

    Add 'getInitials' helper function.

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getInitials(name: string) {
+  
+  if (!name) return 'NN'
+  
+  const cleanName = name.trim();
+  
+  if (cleanName.includes(' ') || cleanName.includes('_') || cleanName.includes('-')) {
+    const parts = cleanName.split(/[\s_-]+/);
+    return parts.map(part => part[0]?.toUpperCase()).join('');
+  }
+  
+  const camelCaseParts = cleanName.match(/[A-Z]?[a-z]+|[A-Z]+(?![a-z])/g);
+  if (camelCaseParts && camelCaseParts.length > 1) {
+    return camelCaseParts.map(part => part[0].toUpperCase()).join('');
+  }
+  
+  return cleanName.slice(0, 2).toUpperCase();
+}
