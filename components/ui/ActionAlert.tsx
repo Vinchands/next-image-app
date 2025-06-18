061aca79aa358c39a36e2354fbae631601922580
Kevin CS <kevinchandra031@gmail.com>
2025-06-18 21:34:07 +0700

    Create 'ActionAlert' to handle actions that need confirmation after a trigger.

---
+++ components/ui/ActionAlert.tsx
@@ -0,0 +1,41 @@
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogCancel,
+  AlertDialogAction
+} from '@/components/ui/alert-dialog'
+
+type ActionAlertProps = {
+  title: string
+  description?: string
+  renderTrigger: React.ReactNode
+  renderAction: React.ReactNode
+}
+
+export default function ActionAlert({
+  title,
+  description,
+  renderTrigger,
+  renderAction
+}: ActionAlertProps) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{renderTrigger}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction asChild>{renderAction}</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
