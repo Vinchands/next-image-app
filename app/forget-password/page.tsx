b8052c873a377c6d9dc7b8cbf6003e85208e7477
Kevin CS <kevinchandra031@gmail.com>
2025-07-04 16:42:22 +0700

    Add padding for better UI.

--- app/forget-password/page.tsx
+++ app/forget-password/page.tsx
@@ -4,7 +4,7 @@ import bg from '@/public/river.jpg'
 export default function ForgetPassword() {
   return (
     <div
-      className="absolute min-h-screen flex items-center justify-center inset-0 mt-14"
+      className="absolute min-h-screen flex items-center justify-center inset-0 p-3 mt-14"
       style={{
         backgroundImage: `url(${bg.src})`,
         backgroundSize: 'cover',
