84cc2d180711587480d9b0459502142017723130
Kevin CS <kevinchandra031@gmail.com>
2025-07-04 16:31:01 +0700

    Modify style for better UI.

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,15 +1,15 @@
 import SigninForm from './SigninForm'
-import bg from '@/public/bg.jpg'
+import bg from '@/public/forest.jpg'
 
 export default function Signup() {
   return (
-    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2 justify-center">
-      <div className="flex items-center justify-center inset-0">
+    <div className="absolute min-h-screen grid grid-cols-1 lg:grid-cols-2 justify-center inset-0 mt-14">
+      <div className="flex items-center justify-center inset-0 p-3 pb-20">
         <SigninForm />
       </div>
       <div
         style={{
-          backgroundImage: `url('${bg.src}')`,
+          backgroundImage: `url(${bg.src})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundAttachment: 'fixed'
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,15 +1,15 @@
 import SignupForm from './SignupForm'
-import bg from '@/public/bg.jpg'
+import bg from '@/public/mountain.jpg'
 
 export default function Signup() {
   return (
-    <div className="min-h-[90vh] grid grid-cols-1 lg:grid-cols-2 justify-center">
-      <div className="flex items-center justify-center inset-0">
+    <div className="absolute min-h-screen grid grid-cols-1 lg:grid-cols-2 justify-center inset-0 mt-14">
+      <div className="flex items-center justify-center inset-0 p-3 pb-20">
         <SignupForm />
       </div>
       <div
         style={{
-          backgroundImage: `url('${bg.src}')`,
+          backgroundImage: `url(${bg.src})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundAttachment: 'fixed'
