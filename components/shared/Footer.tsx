91f7d4333cc03c94beda24c147fbdef585a62cc2
Kevin CS <kevinchandra031@gmail.com>
2025-07-04 16:46:28 +0700

    Add something to the footer.

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
     <footer className="flex items-center justify-between p-5 bg-card border">
-      
+      <div className="">
+        <p className="text-muted-foreground">&copy; 2025 Kevin CS</p>
+      </div>
     </footer>
   )
 }
