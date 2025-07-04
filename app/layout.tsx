86980cb74637f30e27d657d9fccc0490a2568507
Kevin CS <kevinchandra031@gmail.com>
2025-07-04 16:29:53 +0700

    Change font to Poppins and add 'Toaster' component from sonner.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
-import { Mulish } from "next/font/google";
+import { Poppins } from "next/font/google";
 import Navbar from '@/components/shared/Navbar';
 import Footer from '@/components/shared/Footer';
+import { Toaster } from '@/components/ui/sonner';
 import SessionWrapper from '@/components/shared/SessionWrapper';
 import "./globals.css";
 
-const fontSans = Mulish({
+const fontSans = Poppins({
   subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-sans",
 })
 
@@ -26,6 +28,11 @@ export default function RootLayout({
         className={`${fontSans.className} antialiased`}
       >
         <SessionWrapper>
+          <Toaster
+            theme="light"
+            position="bottom-center"
+            richColors
+          />
           <Navbar />
           <main className="max-w-[1536px] container min-h-screen px-3 mx-auto">
             {children}
