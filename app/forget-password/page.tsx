247c86238e9daa03886ae756bae7bcc7aeb67b19
Kevin CS <kevinchandra031@gmail.com>
2025-07-04 16:31:54 +0700

    Update page and implement form.

--- app/forget-password/page.tsx
+++ app/forget-password/page.tsx
@@ -1,7 +1,18 @@
+import ResetPasswordForm from './ResetPasswordForm'
+import bg from '@/public/river.jpg'
+
 export default function ForgetPassword() {
   return (
-    <>
-      Forget password
-    </>
+    <div
+      className="absolute min-h-screen flex items-center justify-center inset-0 mt-14"
+      style={{
+        backgroundImage: `url(${bg.src})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundAttachment: 'fixed'
+      }}
+    >
+      <ResetPasswordForm />
+    </div>
   )
 }
