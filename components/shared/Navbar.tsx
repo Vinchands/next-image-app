d5779809b8eaf6b3fdd64904e695a78228623105
Kevin CS <kevinchandra031@gmail.com>
2025-07-04 16:32:44 +0700

    Add z-index to the navbar for improved UI.

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -6,7 +6,7 @@ import logo from '@/public/logo.svg'
 export default function Navbar() {
   
   return (
-    <header className="sticky top-0 flex items-center justify-between md:justify-around p-3 bg-card shadow">
+    <header className="sticky top-0 z-50 flex items-center justify-between md:justify-around p-3 bg-card shadow">
       <Link href="/" className="flex items-center gap-x-2">
         <Image src={logo} alt="imagenation" className="w-8" />
         <span className="hidden sm:inline font-bold text-2xl">imagenation</span>
