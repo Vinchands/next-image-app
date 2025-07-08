04c6d388722449861a2e35a5715021657739fa62
Kevin CS <kevinchandra031@gmail.com>
2025-07-08 10:57:18 +0700

    Update search bar component for reusability across application.

--- app/SearchBar.tsx
+++ app/SearchBar.tsx
@@ -1,32 +1,47 @@
 'use client'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import { useEffect, useState } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { Search } from 'lucide-react'
-import { useState } from 'react'
-import { useRouter } from 'next/navigation'
+import { cn } from '@/lib/utils'
 
-export default function SearchBar() {
+type SearchBarProps = {
+  basePath?: string
+  className?: string
+  placeholder?: string
+}
+
+export default function SearchBar({ basePath = '/', className, placeholder }: SearchBarProps) {
   
   const router = useRouter()
+  const searchParams = useSearchParams()
+  
   const [query, setQuery] = useState('')
-  const searchURI = `/images?search=${encodeURIComponent(query)}`
+  const searchURI = `${basePath}?q=${encodeURIComponent(query)}`
   
   function handleSearch(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
     router.push(searchURI)
   }
   
+  // * Initialize query from search params on component mount
+  useEffect(() => {
+    const initialQuery = searchParams.get('q') || ''
+    setQuery(initialQuery)
+  }, [searchParams])
+  
   return (
-    <form onSubmit={handleSearch} className="w-full max-w-2xl flex items-center text-black">
+    <form onSubmit={handleSearch} className={cn('flex items-center text-black', className)}>
       <Input
         type="text"
-        placeholder="What are you looking for?"
+        placeholder={placeholder || 'Search...'}
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         className={`bg-card rounded-r-none focus-visible:ring-0 focus-visible:border-0`}
+        required
       />
       <Button
-        variant="secondary"
         type="submit"
         className="rounded-l-none"
         disabled={!query}
