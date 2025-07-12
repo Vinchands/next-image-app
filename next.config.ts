6deedea0efd64d7546d71020c647732437e21f31
Kevin CS <kevinchandra031@gmail.com>
2025-07-12 14:14:13 +0700

    Add remote host for Vercel Storage (Blob).

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.VERCEL_BLOB_HOSTNAME || 'bqf6wroukotlbxwz.public.blob.vercel-storage.com',
+      }
+    ]
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb',
+    }
+  }
 };
 
 export default nextConfig;
