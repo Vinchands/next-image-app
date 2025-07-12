f6171fdf101b816b1dd7b60bf352765874430654
Kevin CS <kevinchandra031@gmail.com>
2025-07-12 21:28:41 +0700

    Add basic sorting feature.

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,34 @@
-import Container from '@/components/ui/container'
-import { H2 } from '@/components/ui/typography'
-import ImageGallery from '@/components/shared/ImageGallery'
 import Hero from './Hero'
-import prisma from '@/lib/prisma'
+import ExploreHeader from './ExploreHeader'
+import Container from '@/components/ui/container'
+import ImageGallery from '@/components/shared/ImageGallery'
+import { getImages } from '@/lib/dal/image.dal'
 
-export default async function Home() {
+export default async function Home({ searchParams }: { searchParams: Promise<{ [key: string]: string | string[] | undefined }> }) {
   
-  const images = await prisma.image.findMany()
+  const params = await searchParams
+  let images = []
+  
+  if (params.sort === 'popular') {
+    images = await getImages({
+      include: {
+        _count: {
+          select: { likes: true }
+        }
+      },
+      orderBy: {
+        likes: { _count: 'desc' }
+      }
+    })
+  } else {
+    images = await getImages()
+  }
   
   return (
     <>
       <Hero />
       <Container size="xl" className="py-5">
-        <section className="flex items-center justify-between gap-x-3 mb-5">
-          <H2>Explore</H2>
-          <p className="text-sm text-muted-foreground">
-            Browse through our collection of free and open source images.
-          </p>
-        </section>
+        <ExploreHeader />
         <ImageGallery images={images} />
       </Container>
     </>
