c9304c57fbbc8f162614c78b10b71e42092da715
Kevin CS <kevinchandra031@gmail.com>
2025-07-12 15:36:10 +0700

    Add container and 'ImageGallery' component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
+import Container from '@/components/ui/container'
+import { H2 } from '@/components/ui/typography'
+import ImageGallery from '@/components/shared/ImageGallery'
 import Hero from './Hero'
+import prisma from '@/lib/prisma'
 
-export default function Home() {
+export default async function Home() {
+  
+  const images = await prisma.image.findMany()
+  
   return (
     <>
       <Hero />
-      <div className="flex items-center justify-between">
-        <h1 className="font-bold text-2xl">Explore</h1>
-        
-      </div>
+      <Container size="xl" className="py-5">
+        <section className="flex items-center justify-between gap-x-3 mb-5">
+          <H2>Explore</H2>
+          <p className="text-sm text-muted-foreground">
+            Browse through our collection of free and open source images.
+          </p>
+        </section>
+        <ImageGallery images={images} />
+      </Container>
     </>
   )
 }
