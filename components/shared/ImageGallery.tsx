09c871533d0cff22786471d68979e9198f93b131
Kevin CS <kevinchandra031@gmail.com>
2025-07-12 21:30:03 +0700

    Add empty state message to 'ImageGallery' component.

--- components/shared/ImageGallery.tsx
+++ components/shared/ImageGallery.tsx
@@ -1,14 +1,32 @@
 import Image from 'next/image'
 import RenderList from '@/components/ui/RenderList'
+import { Button } from '@/components/ui/button'
+import { H3 } from '@/components/ui/typography'
 import { type Image as ImageType } from '@prisma/client'
+import Link from 'next/link'
 
 type ImageGalleryProps = {
   images: ImageType[]
   limit?: number
-  
 }
 
 export default function ImageGallery({ images }: ImageGalleryProps) {
+  
+  if (!images.length) {
+    return (
+      <section className="p-5 text-center border rounded-2xl">
+        <H3 className="mb-3 rounded-2xl">There are no images yet ☹️</H3>
+        <p className="text-muted-foreground">
+          Share your own or{' '}
+          <Button variant="link" className="p-0 text-base" asChild>
+            <Link href="/report">contact us</Link>
+          </Button>
+          {' '}if there is a problem.
+        </p>
+      </section>
+    )
+  }
+  
   return (
     <section className="columns-2 md:columns-3 gap-3">
       <RenderList
