4c972566b2b4fe1640e4741f4cb567781d944f55
Kevin CS <kevinchandra031@gmail.com>
2025-07-15 06:49:07 +0700

    Refactor data filter with the updated version of the get function.

--- app/page.tsx
+++ app/page.tsx
@@ -2,26 +2,24 @@ import Hero from './Hero'
 import ExploreHeader from './ExploreHeader'
 import Container from '@/components/ui/container'
 import ImageGallery from '@/components/shared/ImageGallery'
-import { getImages } from '@/lib/dal/image.dal'
+import { getImagesWithDetail } from '@/lib/dal/image.dal'
+import { ImageDetail } from '@/lib/definitions'
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ [key: string]: string | string[] | undefined }> }) {
   
   const params = await searchParams
-  let images = []
+  let images: ImageDetail[] = []
   
-  if (params.sort === 'popular') {
-    images = await getImages({
-      include: {
-        _count: {
-          select: { likes: true }
-        }
-      },
+  if (params.sort === 'latest') {
+    images = await getImagesWithDetail({
+      orderBy: { updatedAt: 'desc' }
+    })
+  } else {
+    images = await getImagesWithDetail({
       orderBy: {
         likes: { _count: 'desc' }
       }
     })
-  } else {
-    images = await getImages()
   }
   
   return (
