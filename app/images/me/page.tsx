ba2fc70377969b9a26978e0fe824840c57c21cdf
Kevin CS <kevinchandra031@gmail.com>
2025-07-18 08:09:18 +0700

    Create user images page.

---
+++ app/images/me/page.tsx
@@ -0,0 +1,16 @@
+import Container from '@/components/ui/container'
+import ImageGallery from '@/components/shared/ImageGallery'
+import { H1 } from '@/components/ui/typography'
+import { getUserImages } from '@/lib/dal/image.dal'
+
+export default async function Me() {
+  
+  const images = await getUserImages()
+  
+  return (
+    <Container className="py-8">
+      <H1 className="mb-5">My Gallery</H1>
+      <ImageGallery images={images} />
+    </Container>
+  )
+}
