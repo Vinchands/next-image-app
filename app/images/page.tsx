adc07d7de211d0ce2bc885364f13a56b5860fb45
Kevin CS <kevinchandra031@gmail.com>
2025-07-18 08:08:35 +0700

    Add simple display to images page.

--- app/images/page.tsx
+++ app/images/page.tsx
@@ -1,7 +1,20 @@
-export default function Images() {
+import Container from '@/components/ui/container'
+import { H1 } from '@/components/ui/typography'
+import ImageGallery from '@/components/shared/ImageGallery'
+import SearchBar from '@/components/shared/SearchBar'
+import { getImages } from '@/lib/dal/image.dal'
+
+export default async function Images() {
+  
+  const images = await getImages()
+  
   return (
-    <>
-      Images
-    </>
+    <Container size="xl" className="py-8">
+      <div className="space-y-5 mb-5">
+        <H1>Discover</H1>
+        <SearchBar basePath="/images" />
+      </div>
+      <ImageGallery images={images} />
+    </Container>
   )
 }
