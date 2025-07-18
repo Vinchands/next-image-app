33da425dd688ce0c6436072f606f464b6e83c332
Kevin CS <kevinchandra031@gmail.com>
2025-07-18 08:06:46 +0700

    Refactor 'getImages' function and add 'getUserImages'.

--- lib/dal/image.dal.ts
+++ lib/dal/image.dal.ts
@@ -1,8 +1,12 @@
 import prisma from '@/lib/prisma'
 import type { ImageDetail } from '@/lib/definitions'
 import type { Prisma } from '@prisma/client'
+import { getAuthUser } from './user.dal'
+import { cache } from 'react'
 
-export async function getImagesWithDetail(options?: Omit<Prisma.ImageFindManyArgs, 'include'>): Promise<ImageDetail[]> {
+type GetImagesOptions = Omit<Prisma.ImageFindManyArgs, 'include'>
+
+export const getImages = cache(async (options?: GetImagesOptions) => {
   const images = await prisma.image.findMany({
     include: {
       user: {
@@ -16,4 +20,24 @@ export async function getImagesWithDetail(options?: Omit<Prisma.ImageFindManyArg
     ...options
   })
   return images
-}
+})
+
+type WithoutUserIdWhere<T> = T extends { where?: infer W }
+  ? Omit<T, 'where'> & {
+      where?: Omit<W, 'userId'>
+    }
+  : T
+
+type GetUserImagesOptions = WithoutUserIdWhere<GetImagesOptions>
+
+export const getUserImages = cache(async (options?: GetUserImagesOptions) => {
+  const user = await getAuthUser()
+  if (!user) return null
+  
+  const images = await getImages({
+    where: { userId: user.id },
+    ...options
+  })
+  
+  return images
+})
