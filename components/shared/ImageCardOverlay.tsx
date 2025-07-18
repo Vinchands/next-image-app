816cf44b1622e59d008d9a9c8bc6c583e04508c9
Kevin CS <kevinchandra031@gmail.com>
2025-07-18 08:10:25 +0700

    Modify image card overlay to absolute display and add link overlay.

--- components/shared/ImageCardOverlay.tsx
+++ components/shared/ImageCardOverlay.tsx
@@ -1,7 +1,9 @@
 'use client'
+import Link from 'next/link'
 import LikeButton from '@/components/ui/LikeButton'
 import DownloadButton from '@/components/shared/DownloadButton'
 import AuthComponent from '@/components/ui/AuthComponent'
+import { Button } from '@/components/ui/button'
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
 import { getInitials } from '@/lib/utils'
 import { ImageDetail } from '@/lib/definitions'
@@ -10,20 +12,21 @@ import icon from '@/public/user.png'
 export default function ImageCardOverlay({ image }: { image: ImageDetail }) {
   return (
     <div className="absolute inset-0 flex flex-col opacity-0 p-3 group-hover:opacity-100 transition-all ease-in duration-100 bg-black/30 text-white">
-      <div className="flex items-center gap-x-2">
-        <AuthComponent
-          onAuth={() => <LikeButton className="ml-auto" image={image} />}
-          fallback={null}
-        />
-      </div>
-      <div className="flex items-center gap-x-2 mt-auto">
-        <Avatar>
-          <AvatarImage src={image?.user?.photoUrl || icon.src} />
-          <AvatarFallback>{getInitials(image?.user?.name || 'NN')}</AvatarFallback>
-        </Avatar>
-        <p className="max-w-[100px] truncate">{image?.user?.name || 'No user'}</p>
-        <DownloadButton className="ml-auto" />
-      </div>
+      <Link href={`/images/${image.id}`} className="absolute z-0 inset-0" />
+      <AuthComponent
+        onAuth={() => <LikeButton className="absolute top-3 right-3" image={image} />}
+        fallback={null}
+      />
+      <Button variant="ghost" className="absolute bottom-3 left-3 flex items-center gap-x-2 px-1" asChild>
+        <Link href={`/users/${image.userId}`}>
+          <Avatar>
+            <AvatarImage src={image?.user?.photoUrl || icon.src} />
+            <AvatarFallback>{getInitials(image?.user?.name || 'NN')}</AvatarFallback>
+          </Avatar>
+          <p className="max-w-[100px] truncate">{image?.user?.name || 'No user'}</p>
+        </Link>
+      </Button>
+      <DownloadButton className="absolute bottom-3 right-3" />
     </div>
   )
 }
