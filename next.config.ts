b7e1d6b59ee2ec7743b0e983a72bf9d77ce37e06
Kevin CS <kevinchandra031@gmail.com>
2025-07-22 07:30:00 +0700

    Add remote host configuration for Trakteer.

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: process.env.VERCEL_BLOB_HOSTNAME || 'bqf6wroukotlbxwz.public.blob.vercel-storage.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'edge-cdn.trakteer.id'
       }
     ]
   },
