b9a2c571e0dc01ae03ae79d2d0359cb1a11b7847
Kevin CS <kevinchandra031@gmail.com>
2025-07-22 07:32:41 +0700

    Create simple profile page.

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,5 @@
+import Container from '@/components/ui/container'
+import { H1 } from '@/components/ui/typography'
 import { getAuthUser } from '@/lib/dal/user.dal'
 import { toSafeUser } from '@/lib/dto/user.dto'
 
@@ -7,8 +9,9 @@ export default async function Profile() {
   const safeUser = toSafeUser(user!)
   
   return (
-    <>
+    <Container className="py-8">
+      <H1>Profile</H1>
       {safeUser?.name}
-    </>
+    </Container>
   )
 }
