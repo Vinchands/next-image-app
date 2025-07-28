f0a3ce24bd90bbf2cd053ce64aef302fb8b8b70a
Kevin CS <kevinchandra031@gmail.com>
2025-07-28 11:12:21 +0700

    Refactor 'toSafeUser' function to simplify user object creation and directly omit password field for scalability.

--- lib/dto/user.dto.ts
+++ lib/dto/user.dto.ts
@@ -2,13 +2,6 @@ import type { User } from '@prisma/client'
 import type { SafeUser } from '@/lib/definitions'
 
 export function toSafeUser(user: User): SafeUser {
-  return {
-    id: user.id,
-    name: user.name,
-    email: user.email,
-    bio: user.bio,
-    photoUrl: user.photoUrl,
-    createdAt: user.createdAt,
-    updatedAt: user.updatedAt
-  }
+  const safeUser = { ...user, password: undefined }
+  return safeUser
 }
