ee3bfeb72a9f79f27898956ddb2bf508a2aacfd7
Kevin CS <kevinchandra031@gmail.com>
2025-07-28 11:09:45 +0700

    Refactor 'getAuthUser' function to directly omit password field and add options parameter for flexibility.

--- lib/dal/user.dal.ts
+++ lib/dal/user.dal.ts
@@ -3,6 +3,7 @@ import { parseToken } from '@/lib/auth/jwt'
 import { cache } from 'react'
 import { cookies } from 'next/headers'
 import prisma from '@/lib/prisma'
+import { Prisma } from '@prisma/client'
 
 export const verifySession = cache(async () => {
   const token = (await cookies()).get('access-token')?.value
@@ -13,15 +14,15 @@ export const verifySession = cache(async () => {
   return { isAuth: true, userId: payload?.userId }
 })
 
-export const getAuthUser = cache(async () => {
+export const getAuthUser = cache(async (options?: { include: Prisma.UserInclude }) => {
   const session = await verifySession()
   if (!session) return null
   
   try {
     const user = await prisma.user.findFirst({
-      where: {
-        id: session.userId
-      }
+      where: { id: session.userId },
+      omit: { password: true },
+      ...options
     })
     return user
   } catch (err) {
