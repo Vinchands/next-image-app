05266b40ebf9c738474482d39f7968b6e5ffbc33
Kevin CS <kevinchandra031@gmail.com>
2025-07-28 11:10:41 +0700

    Remove 'toSafeUser' function as it is no longer relevant.

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import Container from '@/components/ui/container'
 import { Button } from '@/components/ui/button'
 import { getAuthUser } from '@/lib/dal/user.dal'
-import { toSafeUser } from '@/lib/dto/user.dto'
 import { Metadata } from 'next'
 import { Edit } from 'lucide-react'
 import profile from '@/public/user.png'
@@ -14,15 +13,19 @@ export const metadata: Metadata = {
 
 export default async function Profile() {
   
-  const authUser = await getAuthUser()
-  const user = toSafeUser(authUser!)
+  const user = await getAuthUser({
+    include: {
+      images: true,
+      likes: true,
+    }
+  })
   
   return (
     <Container className="py-8 space-y-6">
       <section className="flex flex-col items-center gap-y-1 p-5">
         <Image src={user?.photoUrl || profile} alt="Profile photo" className="w-36 rounded-full aspect-square" />
-        <h3 className="font-bold text-3xl">{user.name}</h3>
-        <p className="mb-5 text-muted-foreground">{user.email}</p>
+        <h3 className="font-bold text-3xl">{user?.name}</h3>
+        <p className="mb-5 text-muted-foreground">{user?.email}</p>
         <Button asChild>
           <Link href="/profile/edit">
             <Edit /> Edit Profile
