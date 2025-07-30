8273ac0a58d021dbba3ea59253749b37d8a963ab
Kevin CS <kevinchandra031@gmail.com>
2025-07-30 12:54:33 +0700

    Enhance 'TextLink' component to accept additional anchor element props to improve flexibility.

--- components/ui/TextLink.tsx
+++ components/ui/TextLink.tsx
@@ -1,18 +1,18 @@
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
+import React from 'react'
 
-type TextLinkProps = {
+type TextLinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
   href: string
   children: React.ReactNode
   className?: string
-  
 }
 
-export default function TextLink({ href, children, className }: TextLinkProps) {
+export default function TextLink({ href, children, className, ...props }: TextLinkProps) {
   return (
     <Button variant="link" className={cn('h-fit p-0 m-0', className)} asChild>
-      <Link href={href}>
+      <Link href={href} {...props}>
         {children}
       </Link>
     </Button>
