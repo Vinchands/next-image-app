714d159787102bbe6fad4a62faf017f010826c5a
Kevin CS <kevinchandra031@gmail.com>
2025-07-30 13:26:26 +0700

    Refactor profile page component to streamline user authentication and redirection.

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,37 +1,10 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import Container from '@/components/ui/container'
-import { Button } from '@/components/ui/button'
+import { redirect } from 'next/navigation'
 import { getAuthUser } from '@/lib/dal/user.dal'
-import { Metadata } from 'next'
-import { Edit } from 'lucide-react'
-import profile from '@/public/user.png'
-
-export const metadata: Metadata = {
-  title: 'Profile'
-}
 
 export default async function Profile() {
   
-  const user = await getAuthUser({
-    include: {
-      images: true,
-      likes: true,
-    }
-  })
+  const user = await getAuthUser()
   
-  return (
-    <Container className="py-8 space-y-6">
-      <section className="flex flex-col items-center gap-y-1 p-5">
-        <Image src={user?.photoUrl || profile} alt="Profile photo" className="w-36 rounded-full aspect-square" />
-        <h3 className="font-bold text-3xl">{user?.name}</h3>
-        <p className="mb-5 text-muted-foreground">{user?.email}</p>
-        <Button asChild>
-          <Link href="/profile/edit">
-            <Edit /> Edit Profile
-          </Link>
-        </Button>
-      </section>
-    </Container>
-  )
+  if (!user) return redirect('/')
+  redirect(`/profile/${user.username}`)
 }
