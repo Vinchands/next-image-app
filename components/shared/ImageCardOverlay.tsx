0486149e1bfbcaeec11a26e876fdee8b45b354c4
Kevin CS <kevinchandra031@gmail.com>
2025-08-01 19:26:55 +0700

    Refactor 'ImageCardOverlay' for improved functionality.

--- components/shared/ImageCardOverlay.tsx
+++ components/shared/ImageCardOverlay.tsx
@@ -2,13 +2,13 @@
 import Link from 'next/link'
 import LikeButton from '@/components/shared/LikeButton'
 import DownloadButton from '@/components/shared/DownloadButton'
-import AuthComponent from '@/components/ui/AuthComponent'
-import { useRouter } from 'next/navigation'
-import { Button } from '@/components/ui/button'
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import { Heart, Download } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { type ImageDetail } from '@/lib/definitions'
+import profile from '@/public/user.png'
 import { getInitials } from '@/lib/utils'
-import { ImageDetail } from '@/lib/definitions'
-import icon from '@/public/user.png'
 
 export default function ImageCardOverlay({ image }: { image: ImageDetail }) {
   
@@ -16,28 +16,40 @@ export default function ImageCardOverlay({ image }: { image: ImageDetail }) {
   
   function handleClick() {
     if (!image.user) return
-    router.push(`/users/${image.user.id}`)
+    router.push(`/profile/${image.user.username}`)
   }
   
   return (
-    <div className="absolute inset-0 flex flex-col opacity-0 p-3 group-hover:opacity-100 transition-all ease-in duration-100 bg-black/25 text-white">
-      <Link href={`/images/${image.id}`} className="absolute z-0 inset-0" />
-      <AuthComponent
-        onAuth={() => <LikeButton className="hidden md:inline absolute top-3 right-3" image={image} />}
-        fallback={null}
-      />
+    <div className="absolute inset-0 md:hover:bg-black/25 transition-all ease-in duration-100 group">
+      <Link href={`/images/${image.id}`} className="absolute inset-0" />
+      <LikeButton
+        image={image}
+        variant="ghost"
+        size="icon"
+        className={() => 'absolute top-2 right-2 z-10 size-8 max-sm:hidden opacity-0 group-hover:opacity-100 rounded-full'}
+      >
+        {liked => (
+          <Heart className={`size-6 ${liked? 'stroke-destructive fill-destructive' : 'stroke-white'}`} />
+        )}
+      </LikeButton>
+      <DownloadButton
+        image={image}
+        variant="ghost"
+        className="absolute bottom-2 right-2 z-10 size-8 max-sm:hidden opacity-0 group-hover:opacity-100 rounded-full hover:bg-primary"
+      >
+        <Download className="size-6 stroke-white" />
+      </DownloadButton>
       <Button
         variant="ghost"
-        className="absolute bottom-3 left-3 hidden md:inline-flex items-center gap-x-2 px-1"
+        className="absolute bottom-2 left-2 z-10 max-sm:hidden opacity-0 group-hover:opacity-100 p-1 text-white"
         onClick={handleClick}
       >
         <Avatar>
-          <AvatarImage src={image?.user?.photoUrl || icon.src} />
-          <AvatarFallback>{getInitials(image?.user?.name || 'NN')}</AvatarFallback>
+          <AvatarImage src={image.user?.photoUrl || profile.src} />
+          <AvatarFallback>{getInitials(image.user?.name || 'NN')}</AvatarFallback>
         </Avatar>
-        <p className="max-w-[100px] truncate">{image?.user?.name || 'No user'}</p>
+        {image.user?.name}
       </Button>
-      <DownloadButton className="absolute bottom-3 right-3 hidden md:inline" />
     </div>
   )
 }
