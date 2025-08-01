8fbddfccdd0a1e7bb50b2bf0af0581038599fa81
Kevin CS <kevinchandra031@gmail.com>
2025-08-01 19:26:22 +0700

    Enhance 'ImageCard' component with improved styling and isolation.

--- components/shared/ImageCard.tsx
+++ components/shared/ImageCard.tsx
@@ -1,10 +1,11 @@
+// 'use client'
 import Image from 'next/image'
 import ImageCardOverlay from '@/components/shared/ImageCardOverlay'
 import type { ImageDetail } from '@/lib/definitions'
 
 export default function ImageCard({ image }: { image: ImageDetail }) {
   return (
-    <div className="relative w-full mb-3 bg-secondary rounded-2xl **:rounded-2xl group">
+    <div className="relative isolate w-full mb-3 bg-secondary rounded-2xl **:rounded-2xl group">
       <ImageCardOverlay image={image} />
       <Image
         src={image.previewUrl}
@@ -13,7 +14,7 @@ export default function ImageCard({ image }: { image: ImageDetail }) {
         height={500}
         placeholder="blur"
         blurDataURL={image.blurUrl}
-        className="rounded-xl object-cover pointer-events-none"
+        className="w-full rounded-xl pointer-events-none"
       />
     </div>
   )
